c1f34c103458de6a474b1cbcecfb128cd11f99ca
Lepp3 <lubaka05@gmail.com>
2025-02-26 16:37:52 +0200

    added practice server resources

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import TodoList from './components/TodoList'
 
 function App() {
  
@@ -16,60 +17,9 @@ function App() {
 
   
   <main className="main">
-
+    <TodoList/>
     
-    <section className="todo-list-container">
-      <h1>Todo List</h1>
-
-      <div className="add-btn-container">
-        <button className="btn">+ Add new Todo</button>
-      </div>
-
-      <div className="table-wrapper">
-
-        {/* <!-- Loading spinner - show the load spinner when fetching the data from the server--> */}
-        {/* <div className="loading-container">
-          <div className="loading-spinner">
-            <span className="loading-spinner-text">Loading</span>
-          </div>
-        </div> */}
-
-        {/* <!-- Todo list table --> */}
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="table-header-task">Task</th>
-              <th className="table-header-status">Status</th>
-              <th className="table-header-action">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo is-completed">
-              <td>Give dog a bath</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            <tr className="todo">
-              <td>Vacuum floor</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            
-            
-
-            
-          </tbody>
-        </table>
-      </div>
-    </section>
+   
   </main>
 
   
--- client/src/components/TodoListItem.jsx
+++ client/src/components/TodoListItem.jsx
@@ -1,5 +1,11 @@
 export default function TodoListItem(){
     return(
-        <li></li>
+        <tr className="todo is-completed">
+                <td>Give dog a bath</td>
+                <td>Complete</td>
+                <td className="todo-action">
+                  <button className="btn todo-btn">Change status</button>
+                </td>
+              </tr>
     )
 }
